31803a713d2b53d9af2dbe8e52ee7fb97ac297fc
Juciano Victor <jucianovictor95@gmail.com>
2020-12-18 01:17:13 -0300

    ops, isso nunca aconteceu

--- src/app/pages/home/home.module.ts
+++ src/app/pages/home/home.module.ts
@@ -5,21 +5,17 @@ import { SharedModule } from "../../shared/shared.module";
 import { HomeRoutingModule } from './home-routing.module';
 import { HomeComponent } from './home.component';
 
-import { NgbCarouselModule } from '@ng-bootstrap/ng-bootstrap';
-import { BsCarouselComponent } from './components/bs-carousel/bs-carousel.component';
 import { MainSliderComponent } from './components/main-slider/main-slider.component';
 
 @NgModule({
   declarations: [
     HomeComponent,
-    BsCarouselComponent,
     MainSliderComponent
   ],
   imports: [
     CommonModule,
     HomeRoutingModule,
-    SharedModule,
-    NgbCarouselModule
+    SharedModule
   ]
 })
 export class HomeModule { }
