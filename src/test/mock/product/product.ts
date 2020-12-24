ccb3f4b7d5cdf1764939b505a0fbab22500fa68e
Juciano Victor <jucianovictor95@gmail.com>
2020-12-24 12:16:46 -0300

    Ajuste no mock de product

--- src/test/mock/product/product.ts
+++ src/test/mock/product/product.ts
@@ -1,3 +1,4 @@
+import { Genre } from "src/app/core/models/enum/genre.enum";
 import { Product } from "../../../app/core/models/dto/product";
 import { ProductCategory } from "../../../app/core/models/enum/product-category.enum";
 
@@ -6,70 +7,82 @@ export let mockedProducts: Product[] = [
     id: 0,
     name: 'Camisa',
     category: ProductCategory.T_SHIRT,
-    color: {
+    colors: [{
       name: 'Yellow',
       value: '#ffff00'
-    },
+    }],
+    genre: Genre.MALE,
     price: 200
   },
   {
     id: 1,
     name: 'Supreme T-Shirt',
     category: ProductCategory.T_SHIRT,
-    color: {
+    colors: [{
       name: 'Purple',
       value: '#ff00ff'
-    },
+    }],
+    genre: Genre.MALE,
     price: 100
   },
   {
     id: 2,
     name: 'Nike Air 99.0',
     category: ProductCategory.SNEAKERS,
-    color: {
+    colors: [{
       name: 'Blue',
       value: '#0000ff'
-    },
+    }],
+    genre: Genre.MALE,
     price: 99.50
   },
   {
     id: 3,
     name: 'BR Camisa',
     category: ProductCategory.T_SHIRT,
-    color: {
+    colors: [{
       name: 'Red',
       value: '#ff0000'
-    },
+    }],
+    genre: Genre.MALE,
     price: 700
   },
   {
     id: 4,
     name: 'NA Camisa',
     category: ProductCategory.T_SHIRT,
-    color: {
-      name: 'Red',
-      value: '#ff0000'
+    colors: [{
+      name: 'Yellow',
+      value: '#ffff00'
     },
+    {
+      name: 'Green',
+      value: '#00ff00'
+    }
+  ],
+    genre: Genre.MALE,
     price: 100
   },
   {
     id: 5,
     name: 'X0 Camisa',
     category: ProductCategory.T_SHIRT,
-    color: {
+    colors: [{
       name: 'Red',
       value: '#ff0000'
-    },
+    }],
+    genre: Genre.MALE,
     price: 100
   },
   {
-    id: 5,
+    id: 6,
     name: 'YE Camisa',
     category: ProductCategory.T_SHIRT,
-    color: {
+    colors: [{
       name: 'Yellow',
       value: '#ffff00'
-    },
+    }],
+    genre: Genre.MALE,
     price: 100
   }
 ]
