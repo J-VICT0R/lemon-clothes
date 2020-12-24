5518fc0a57c184bff9da94e898704e23a98b9222
Juciano Victor <jucianovictor95@gmail.com>
2020-12-24 11:48:44 -0300

    Adição de genre e ajustando color para colorS

--- src/app/core/models/dto/product.ts
+++ src/app/core/models/dto/product.ts
@@ -1,10 +1,12 @@
 import { ProductCategory } from '../enum/product-category.enum'
 import { Color } from '../dto/color'
+import { Genre } from '../enum/genre.enum'
 
 export interface Product {
   id: number
   name: string
   price: number
-  color: Color
+  colors: Color[]
+  genre: Genre
   category: ProductCategory
 }
---
+++ src/app/core/models/enum/genre.enum.ts
@@ -0,0 +1,5 @@
+export enum Genre {
+  MALE = 'Male',
+  FEMALE = 'Female',
+  ALL = 'All'
+}
